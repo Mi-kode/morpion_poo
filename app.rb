988e59e3ceb44bb02d436ab0d39c295723b429bd
Mi-kode <folan.t@hotmail.com>
2025-06-03 11:34:13 +0200

    Last update, bug with icon switch to correct

--- app.rb
+++ app.rb
@@ -3,12 +3,10 @@ Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
-require_relative 'lib/board'
 
 # Initialisation
-icons = ["🔷", "🔴"]
 
-puts "Bienvenue dans ❌ Tic-Tac-Toe ⭕ !"
+puts "Bienvenue dans 🔷 Tic-Tac-Toe 🔴 !"
 puts"\nJoueur 1 :"
 print ">> "
 pseudo1 = gets.chomp
@@ -17,8 +15,50 @@ puts"\nJoueur 2 :"
 print ">> "
 pseudo2 = gets.chomp
 
-my_game = Game.new(pseudo1, pseudo2, icons)
+round = 0
 
-puts "\n#{my_game.player1.name} => #{my_game.player1.icon}"
-puts "VS"
-puts "#{my_game.player2.name} => #{my_game.player2.icon}"
+loop do
+    
+    icons = ["🔷", "🔴"]
+
+    my_game = Game.new(pseudo1, pseudo2, icons)
+
+    puts "\n#{my_game.player1.name} => #{my_game.player1.icon}"
+    puts "VS"
+    puts "#{my_game.player2.name} => #{my_game.player2.icon}"
+
+    # Jeu en cours
+
+    leave = false
+
+    puts "\nRound #{round}"
+    
+    round += 1
+
+    while !my_game.victory? && !my_game.full_board?
+        my_game.display_board
+        my_game.menu
+        action = gets.chomp
+        leave = true if my_game.menu_choice(action) == :leave
+        break if leave
+    end
+
+    # Fin de game
+
+    if leave
+        puts "\nÀ bientôt ! 👋"
+        break
+    elsif my_game.victory?
+        my_game.display_board
+        winner = (my_game.current_player == my_game.player1 ? my_game.player2 : my_game.player1)
+        puts "\n#{winner.name} a gagné ! 🎉"
+    else
+        puts "\nMatch nul !"
+    end
+
+    puts "\nSouhaitez-vous rejouer ? (y/n)"
+    print "> "
+    restart = gets.chomp.downcase
+    break unless restart == "y"
+
+end
--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 class Game
-    attr_accessor :player1, :player2
+    attr_accessor :player1, :player2, :casechoice, :current_player
 
     def initialize(pseudo1, pseudo2, icons)
         tag1 = icons.sample
@@ -9,6 +9,73 @@ class Game
         tag2 = icons.first
         @player1 = Player.new(pseudo1, tag1)
         @player2 = Player.new(pseudo2, tag2)
+        @casechoice = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        @current_player = @player1
+    end
+
+    def menu
+        puts "\n-------------------------------------"
+        puts "| Au tour de #{@current_player.name} (#{@current_player.icon})"
+        puts "| Choisis une case disponible parmi : #{@casechoice.select { |c| c.is_a?(Integer) }.join(', ')}"
+        puts "| Quitter en entrant 'leave'"
+        puts "-------------------------------------"
+        print "> "
+    end
+
+    def menu_choice(action)
+        return :leave if action == "leave"
+
+        choice = action.to_i
+
+        if @casechoice.include?(choice)
+            index = @casechoice.index(choice)
+            @casechoice[choice - 1] = @current_player.icon
+            switch_turn unless victory?
+        else
+            puts "\nChoix invalide.. Réessaie."
+        end
+    end
+
+    def display_board
+        puts ""
+        puts "      |      |      "
+        puts "  #{@casechoice[0]}  |  #{@casechoice[1]}  |  #{@casechoice[2]}  "
+        puts "______|______|______"
+        puts "      |      |      "
+        puts "  #{@casechoice[3]}  |  #{@casechoice[4]}  |  #{@casechoice[5]}  "
+        puts "______|______|______"
+        puts "      |      |      "
+        puts "  #{@casechoice[6]}  |  #{@casechoice[7]}  |  #{@casechoice[8]}  "
+        puts "      |      |      "
+        puts ""
+    end
+
+    def switch_turn
+        @current_player = (@current_player == @player1 ? @player2 : @player1)
+    end
+
+    def victory?
+        winning_combos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # lignes
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # colonnes
+            [0, 4, 8], [2, 4, 6]              # diagonales
+        ]
+
+        winning_combos.each do |combo|
+            a, b, c = combo
+            if
+            @casechoice[a] == @casechoice[b] &&
+            @casechoice[b] == @casechoice[c] &&
+            @casechoice[a].is_a?(String)
+                return true
+            end
+        end
+
+        false
+    end
+
+    def full_board?
+        @casechoice.none? { |c| c.is_a?(Integer) }
     end
 
 end
